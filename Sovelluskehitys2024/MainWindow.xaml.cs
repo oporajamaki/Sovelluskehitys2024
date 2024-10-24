f5ef6897e3254acecf35a0c8151a6133bb8e2132
juhay-git <juha.yli-hemminki@seamk.fi>
2024-10-24 12:02:31 +0300

    Tietokantayhteys lisätty

--- Sovelluskehitys2024/MainWindow.xaml.cs
+++ Sovelluskehitys2024/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
+using Microsoft.Data.SqlClient;
 
 namespace Sovelluskehitys2024
 {
@@ -23,8 +26,22 @@ namespace Sovelluskehitys2024
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            tekstikentta.Clear();
-            ((Button)sender).Content = "Klikattu"; 
+            string polku = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\k5000833\\Documents\\testitietokanta.mdf;Integrated Security=True;Connect Timeout=30";
+            SqlConnection yhteys = new SqlConnection(polku);
+            yhteys.Open();
+
+            string kysely = "SELECT * FROM tuotteet";
+            SqlCommand komento = yhteys.CreateCommand();
+            komento.CommandText = kysely;
+
+            SqlDataAdapter adapteri = new SqlDataAdapter(komento);
+            DataTable dt = new DataTable("tuotteet");
+            adapteri.Fill(dt);
+
+            tuotelista.ItemsSource = dt.DefaultView;
+
+            yhteys.Close();
+
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
---
+++ Sovelluskehitys2024/SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE tuotteet (id INTEGER IDENTITY(1,1) PRIMARY KEY, nimi TEXT, hinta INTEGER);
+
+INSERT INTO tuotteet (nimi, hinta) VALUES ('juusto', 6);
+
+SELECT * FROM tuotteet;
