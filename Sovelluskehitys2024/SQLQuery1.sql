0e4de7021743c2e2e99d0643608263bb7d27c13e
juhay-git <juha.yli-hemminki@seamk.fi>
2024-11-04 13:45:47 +0200

    Widgetit siirretty TabControlliin

--- Sovelluskehitys2024/SQLQuery1.sql
+++ Sovelluskehitys2024/SQLQuery1.sql
@@ -1,10 +1,18 @@
-﻿CREATE TABLE tuotteet (id INTEGER IDENTITY(1,1) PRIMARY KEY, nimi TEXT, hinta INTEGER);
+﻿CREATE TABLE tuotteet (id INTEGER IDENTITY(1,1) PRIMARY KEY, nimi VARCHAR(50), hinta INTEGER);
 
-CREATE TABLE tuotteet (id INTEGER IDENTITY(1,1) PRIMARY KEY, nimi VARCHAR(50), hinta INTEGER);
+CREATE TABLE asiakkaat (id INTEGER IDENTITY(1,1) PRIMARY KEY, nimi VARCHAR(50), osoite VARCHAR(150), puhelin VARCHAR(50));
+
+CREATE TABLE tilaukset (id INTEGER IDENTITY(1,1) PRIMARY KEY, asiakas_id INTEGER REFERENCES asiakkaat ON DELETE CASCADE, tuote_id INTEGER REFERENCES tuotteet ON DELETE CASCADE);
 
 INSERT INTO tuotteet (nimi, hinta) VALUES ('juusto', 6);
+INSERT INTO asiakkaat (nimi, osoite, puhelin) VALUES ('Masa', 'Kuusikuja 6', '050882682');
+INSERT INTO tilaukset (asiakas_id, tuote_id) VALUES (2,5); 
+
+DELETE FROM tuotteet WHERE id=5;
 
 SELECT * FROM tuotteet;
+SELECT * FROM asiakkaat;
+SELECT * FROM tilaukset;
 
 DELETE FROM tuotteet WHERE nimi="kinkku";
 
