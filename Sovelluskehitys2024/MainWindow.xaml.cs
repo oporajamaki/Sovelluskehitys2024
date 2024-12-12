47bb6cc52334880e5b8f601c2802bc10134ba123
oporajamaki <oporajamaki@gmail.com>
2024-12-12 16:39:49 +0200

    Combobox

--- Sovelluskehitys2024/MainWindow.xaml.cs
+++ Sovelluskehitys2024/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.Reflection;
 using System.Text;
 using System.Windows;
@@ -21,7 +22,10 @@ namespace Sovelluskehitys2024
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
-        string polku = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\k2201452\\Documents\\levykauppa.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True";
+        /// <summary>
+        /// string polku = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\k2201452\\Documents\\levykauppa.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True";
+        /// </summary>
+        string polku = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Käyttäjä\\OneDrive\\Tiedostot\\levykauppa.mdf;Integrated Security = True; Connect Timeout = 30;Encrypt=True";
         public MainWindow()
         {
             InitializeComponent();
@@ -32,8 +36,9 @@ namespace Sovelluskehitys2024
             {
                 PaivitaDataGrid("SELECT * FROM tuotteet", "tuotteet", tuotelista);
                 PaivitaDataGrid("SELECT * FROM asiakkaat", "asiakkaat", asiakaslista);
-                PaivitaDataGrid("SELECT ti.id as id, a.nimi as asiakas, tu.nimi as tuote FROM tilaukset ti, asiakkaat a, tuotteet tu WHERE a.id=ti.asiakas_id AND tu.id=ti.tuote_id AND ti.toimitettu='0'", "tilaukset", tilauslista);
-                PaivitaDataGrid("SELECT ti.id as id, a.nimi as asiakas, tu.nimi as tuote FROM tilaukset ti, asiakkaat a, tuotteet tu WHERE a.id=ti.asiakas_id AND tu.id=ti.tuote_id AND ti.toimitettu='1'", "tilaukset", toimitetutlista);
+                // toimiiko tämä
+                //PaivitaDataGrid("SELECT ti.id as id, a.nimi as asiakas, tu.nimi as tuote FROM tilaukset ti, asiakkaat a, tuotteet tu WHERE a.id=ti.asiakas_id AND tu.id=ti.tuote_id AND ti.toimitettu='0'", "tilaukset", tilauslista);
+                //PaivitaDataGrid("SELECT ti.id as id, a.nimi as asiakas, tu.nimi as tuote FROM tilaukset ti, asiakkaat a, tuotteet tu WHERE a.id=ti.asiakas_id AND tu.id=ti.tuote_id AND ti.toimitettu='1'", "tilaukset", toimitetutlista);
                 PaivitaComboBox(tuotelista_cb, tuotelista_cb_2);
                 PaivitaAsiakasComboBox();
             }
@@ -73,21 +78,24 @@ namespace Sovelluskehitys2024
             DataTable taulu = new DataTable();
             taulu.Columns.Add("ID",  typeof(string));
             taulu.Columns.Add("NIMI", typeof(string));
+            taulu.Columns.Add("NIMI_LEVY", typeof(string));
+
 
             /* tehdään sidokset että comboboxissa näytetää datataulua*/
             kombo1.ItemsSource = taulu.DefaultView;
-            kombo1.DisplayMemberPath = "NIMI";
+            kombo1.DisplayMemberPath = "NIMI_LEVY";
             kombo1.SelectedValuePath = "ID";
-
+            
             kombo2.ItemsSource = taulu.DefaultView;
-            kombo2.DisplayMemberPath = "NIMI";
+            kombo2.DisplayMemberPath = "NIMI_LEVY";
             kombo2.SelectedValuePath = "ID";
 
             while (lukija.Read()) // käsitellään kyselytulos rivi riviltä
             {
                 int id = lukija.GetInt32(0); 
                 string nimi = lukija.GetString(1);
-                taulu.Rows.Add(id, nimi); // lisätään datatauluun rivi tietoineen
+                string nimi2 = lukija.GetString(2);
+                taulu.Rows.Add(id, nimi, $"{nimi} - {nimi2}"); // lisätään datatauluun rivi tietoineen
                 //tuotelista_cb.Items.Add(lukija.GetString(1));
             }
             lukija.Close();
